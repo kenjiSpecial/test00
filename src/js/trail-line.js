c3b2b82896979969569cb4b949eb1c0f3bbcb317
Kenji Saito <k.saito.1985@gmail.com>
2016-11-05 23:51:27 -0700

    update prototype00

---
+++ src/js/trail-line.js
@@ -0,0 +1,14 @@
+"use strict";
+const THREE = require('three');
+
+export default class TraiLine extends THREE.Mesh {
+    constructor() {
+        var verticeNum = 100;
+
+        var geo = new THREE.Geometry()
+
+        super(geo, mat)
+    }
+    createLine()
+
+}
